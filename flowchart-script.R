8f20f8f537be49557dcb8f6b3e2016d253453e05
Ilse Pit <ilsepit@gmail.com>
2020-01-11 18:29:05 +0000

    a first adjustment of the example script in order to create the final flowchart. Elements taken from google doc made during #repro20. Seperated the input for labels and edges.

--- flowchart-script.R
+++ flowchart-script.R
@@ -1,44 +1,112 @@
 install.packages("DiagrammeR")
 library(DiagrammeR)
 
-grViz("digraph flowchart {
-      # node definitions with substituted label text
-      node [fontname = Helvetica, shape = rectangle]        
-      tab1 [label = '@@1']
-      tab2 [label = '@@2']
-      tab3 [label = '@@3']
-      tab4 [label = '@@4']
-      tab5 [label = '@@5']
+# Elements that are in the flowchart
+collabQ = "Are there any collaborators on this project?"
+collab_Y1 = "Discuss authorship"
+collab_Y2 = "Discuss alocation of tasks"
 
-      # edge definitions with the node IDs
-      tab1 -> tab2 -> tab3 -> tab4 -> tab5;
-      }
+osf = "Create OSF project"
+osf1 = paste("Use OSF project template, see", osf1_link, "for an example")
+osf1_link = "[add link]"
+osf2 = "Add collaborators to OSF project"
 
-      [1]: 'Questionnaire sent to n=1000 participants'
-      [2]: 'Participants responded to questionnaire n=850'
-      [3]: 'Participants came to clinic for evaluation n=700'
-      [4]: 'Participants eligible for the study n=600'
-      [5]: 'Study sample n=600'
-      ")
+dataMan = "Create a data management plan"
+dataMan1 = paste("See", dataMan1_link, "for an online platform to create your plan")
+dataMan1_link = "https://dmponline.dcc.ac.uk/"
 
-grViz("digraph flowchart {
-      # node definitions with substituted label text
-      node [fontname = Helvetica, shape = rectangle]        
-      tab1 [label = '@@1']
-      tab2 [label = '@@2']
-      tab3 [label = '@@3']
-      tab4 [label = '@@4']
-      tab5 [label = '@@5']
+repo = "Set up a repository"
+repo1 = paste("Examples of repositories are GitHub/GitLab/Bitbucket", repo1_link)
+repo1_link = "[add link to template or example]"
+repo2 = "Add collaborators to repository"
+repo3 = paste("Link repository to OSF as shown in", repo3_link)
+repo3_link = "[link to how to add repo to osf]"
+repro4 = "Determine sharability of data [fix]"
 
-      # edge definitions with the node IDs
-      tab1 -> tab2;
-      tab2 -> tab3;
-      tab2 -> tab4 -> tab5
-      }
+jour = "Consider journals"
+jour1 = paste("see", jour1_link, "for an overview of registered report journals")
 
-      [1]: 'Questionnaire sent to n=1000 participants'
-      [2]: 'Participants came to clinic for evaluation n=700'
-      [3]: 'Participants non-eligible for the study n=100'
-      [4]: 'Participants eligible for the study n=600'
-      [5]: 'Study sample n=600'
-      ")
+lit = "Literature review"
+lit1 = "Read a ton"
+lit2 = "Ask authors for e.g. effect sizes, non-open data"
+
+# Theoretical framework write up  (Introduction)
+# Define variables
+# Determine population 
+# Form hypotheses & study design
+# Write down H0 & Ha per hypotheses
+# Determine data structure
+# Overview of type of variables and ranges
+# Simulation
+# Determine analysis: (how will I answer my RQ)
+# Subflow chart?
+#    Inclusion/Exclusion criteria
+# Prepare analysis: (what to do before I can do my analysis) 
+# Create script for data cleaning
+# Decide how to handle missing data / outliers
+# Create script for analysis (including assumption checks)
+# Test analysis script with dummy data
+# Sample size determination
+# Power analysis
+# Simulate data
+# Create experiment
+# Choose or create stimuli
+# Choose measuring tool variables
+# Determine procedure
+# Pilot above elements
+# Choose sample
+# Write up methods
+# Submit for ethical review (IRB)
+# After approval, continue on 
+# If changes are necessary, go through steps 7 - 14 again as necessary
+# (preprint at this stage?)
+# Preregistration / submit stage 1 registered report
+# Preregister everything up to and including ethics
+# Data collection
+# Analysis
+# Clean / anonymize data
+# Upload data on OSF
+# Run preregistered preparation and analysis (point 8)
+# Exploratory analysis
+# Write up results and discussion
+# Preprint authors’ version (Open Access Publishing - UKRN Primers)
+# Publish / Stage 2 Registered Report 
+# 
+
+# Definitions of shape of flow chart
+# node definitions with substituted label text
+nodeDef =
+"node [fontname = Helvetica, shape = rectangle]        
+tab1 [label = '@@1']
+tab2 [label = '@@2']
+tab3 [label = '@@3']
+tab4 [label = '@@4']
+tab5 [label = '@@5']
+"
+
+# Definitions of connections (edges) between elements
+# edge definitions with the node IDs
+edgeDef = 
+"tab1 -> tab2 -> tab3 -> tab4 -> tab5;
+tab1 -> tab4;
+"
+
+labNames = paste0(
+"
+[1]: '", collabQ,"'
+[2]: '", collab_Y1,"'
+[3]: '", collab_Y2,"'
+[4]: '", osf,"'
+[5]: '", osf1,"'
+"
+)
+
+grViz(
+   paste("digraph flowchart {",
+         nodeDef,
+         edgeDef,
+         "}", 
+         labNames
+         )
+   )
+
